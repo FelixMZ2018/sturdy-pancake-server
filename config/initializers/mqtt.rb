d899ccabbc9be5ee14d6cb7c30ae56b85dd60c23
Felix <FelixinLondon@googlemail.com>
2020-10-18 16:47:25 +0200

    CHANGE: Moving MQTT Logic to separate file

---
+++ config/initializers/mqtt.rb
@@ -0,0 +1,65 @@
+puts "Starting MQTT Connection"
+
+Thread.new do
+    MQTT::Client.connect('192.168.0.26') do |c|
+      # If you pass a block to the get method, then it will loop
+      c.get('Plants/#') do |topic,message|
+        puts "#{topic}: #{message}"
+        reading = JSON.parse(message)
+        plant = reading["Plant"]
+        groupname = reading["PlantGroup"]
+        hardware_id = reading["Sensor_ID"]
+        sensor_value = reading["SensorValue"]
+        if !verifyGroup?(groupname) 
+          Group.create(name: groupname)
+        end
+        unless plant == nil
+          if !verifyPlant?(plant)
+            Plant.create(name: plant,group_id: Group.find_by(name: groupname).id)
+          end
+        end
+        if !verifySensor?(hardware_id)
+          Sensor.create(hardware_id: hardware_id,plant_id: (plant == nil ? nil : Plant.find_by(name: plant).id),group_id: Group.find_by(name: groupname).id)
+        end
+        Datapoint.create(sensor_id: Sensor.find_by(hardware_id: hardware_id).id,value: sensor_value)
+      end
+    end
+  end
+  
+  def verifyGroup?(groupname)
+    if GROUP_ARRAY.include?(groupname)
+      return true
+    elsif !Group.where(name: groupname).empty?
+      GROUP_ARRAY << groupname
+      return true
+    else
+      GROUP_ARRAY << groupname
+      return false
+    end
+  end
+  
+  def verifySensor?(id)
+    p id
+    if SENSOR_ARRAY.include?(id)
+      return true
+    elsif !Sensor.where(hardware_id: id).empty?
+      SENSOR_ARRAY << id
+      return true
+    else
+      SENSOR_ARRAY << id
+      return false
+    end
+  end
+  
+  def verifyPlant?(name)
+    if PLANT_ARRAY.include?(name)
+      return true
+    elsif !Plant.where(name: name).empty?
+      PLANT_ARRAY << name
+      return true
+    else
+      PLANT_ARRAY << name
+      return false
+    end
+  end
+  
