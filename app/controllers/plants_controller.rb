c90b4c2bd7a7124d35169cbf6ec2f8e0b46066ba
Felix <FelixinLondon@googlemail.com>
2020-10-21 20:59:43 +0200

    Added Plant Controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,16 @@
 class PlantsController < ApplicationController
+    def edit
+        @Plant = Plant.find(params[:id])
+    end
+
+    def update
+        @Plant = Plant.find(params[:id])
+        if @Plant.update(params)
+          redirect_to @Plant, notice: 'Cocktail was successfully updated.'
+        else
+          render :edit
+        end
+      end
+    
+    
 end
